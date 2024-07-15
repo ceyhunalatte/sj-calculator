93a8b3a3c41c0d4b4c7fb68ad34567a2f44b6342
Ceyhun Carpar <ceyhuncarpar@gmail.com>
2024-07-15 11:50:48 +0300

    fix android colors & final review

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = ({  }) => (
-  <footer className='h-16 bg-[#232323] max-w-screen px-4 py-2 flex items-end'>
+  <footer className='h-16 bg-[#232323] max-w-screen px-4 py-2 flex items-center '>
     <p className='text-center font-light text-gray-2000 text-[14px]'>Stable Jack take home interview project</p>
   </footer>  
 );
--- src/app/components/trading.tsx
+++ src/app/components/trading.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react';
 import Input from '@/components/input';
 import useTradingStore from '@/store/tradeStore';
